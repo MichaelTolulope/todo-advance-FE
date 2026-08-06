bda44b562e0e50fd394a3e22f7f8d8a9310e0941
Michael Tolulope <olagunjum99@gmail.com>
2026-08-06 20:16:09 +0100

    added groom sdk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script'
 
 const geistSans = Geist({
   variable: "--font-sans",
@@ -27,7 +28,14 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">{children}
+ <Script
+          src="https://sdk.usegroom.com/index.global.js"
+          groom-sdk-key="ws_sdk_093c0b4cab3bbc8c"
+          groom-server="aHR0cHM6Ly90b2RvLWFkdmFuY2UtYmUudmVyY2VsLmFwcA=="
+          strategy="lazyOnload"
+        />
+      </body>
     </html>
   );
 }
